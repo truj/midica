d80ccadf5e6531b25bbfc7d0f356da1015c4df35
Jan Trukenmüller <truj@users.noreply.github.com>
2015-12-16 16:00:56 +0100

    new version

--- src/org/midica/Midica.java
+++ src/org/midica/Midica.java
@@ -22,7 +22,7 @@ public class Midica {
 	public static final int VERSION_MAJOR = 0;
 	
 	/** Minor version number. This is intended to be incremented automatically by precommit.pl. */
-	public static final int VERSION_MINOR = 1449773727;
+	public static final int VERSION_MINOR = 1450278056;
 	
 	/** Full version string. */
 	public static final String VERSION = VERSION_MAJOR + "." + VERSION_MINOR;
