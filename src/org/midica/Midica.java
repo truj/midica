b058da641b731d28cea892393faf19e1b05bd585
Jan Trukenmüller <truj@users.noreply.github.com>
2019-11-17 22:35:37 +0100

    added example song

--- src/org/midica/Midica.java
+++ src/org/midica/Midica.java
@@ -25,7 +25,7 @@ public class Midica {
 	public static final int VERSION_MAJOR = 0;
 	
 	/** Minor version number. This is intended to be incremented automatically by precommit.pl. */
-	public static final int VERSION_MINOR = 1573924993;
+	public static final int VERSION_MINOR = 1574026540;
 	
 	/** Full version string. */
 	public static final String VERSION = VERSION_MAJOR + "." + VERSION_MINOR;
