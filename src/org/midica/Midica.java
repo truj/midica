5b1f6220ed8e987d22408ec748919714b28e1ba8
Jan Trukenmüller <truj@users.noreply.github.com>
2020-05-06 15:58:02 +0200

    corrected wrong headers in automated tests
    - #64

--- src/org/midica/Midica.java
+++ src/org/midica/Midica.java
@@ -26,7 +26,7 @@ public class Midica {
 	public static final int VERSION_MAJOR = 0;
 	
 	/** Minor version number. This is intended to be incremented automatically by precommit.pl. */
-	public static final int VERSION_MINOR = 1588772099;
+	public static final int VERSION_MINOR = 1588773485;
 	
 	/** Full version string. */
 	public static final String VERSION = VERSION_MAJOR + "." + VERSION_MINOR;
