6e4aef26e1495b1a12f923c601fa3f3c5bba3417
Jan Trukenmüller <truj@users.noreply.github.com>
2021-06-01 22:41:35 +0200

    new version after merging the export-formats branch

--- src/org/midica/Midica.java
+++ src/org/midica/Midica.java
@@ -33,13 +33,13 @@ public class Midica {
 	 * After switching to a new major version, this has to be set to "-1" manually, so that
 	 * precommit.pl starts with "0" again.
 	 */
-	private static final int VERSION_MINOR = -1;
+	private static final int VERSION_MINOR = 0;
 	
 	/** UNIX timestamp of the last commit */
-	public static final int COMMIT_TIME = 1622579750;
+	public static final int COMMIT_TIME = 1622580103;
 	
 	/** Branch name. Automatically changed by precommit.pl */
-	public static final String BRANCH = "export-formats";
+	public static final String BRANCH = "master";
 	
 	/** Full version string. */
 	public static final String VERSION = VERSION_MAJOR + "." + VERSION_MIDDLE + "." + VERSION_MINOR + ("master".equals(BRANCH) ? "" : "-" + BRANCH);
