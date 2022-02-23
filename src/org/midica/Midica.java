12aac42aec10ee98f3deee07227edd078de0e93f
Jan Trukenmüller <truj@users.noreply.github.com>
2022-02-23 21:33:56 +0100

    updated version

--- src/org/midica/Midica.java
+++ src/org/midica/Midica.java
@@ -33,13 +33,13 @@ public class Midica {
 	 * After switching to a new major version, this has to be set to "-1" manually, so that
 	 * precommit.pl starts with "0" again.
 	 */
-	private static final int VERSION_MINOR = 1;
+	private static final int VERSION_MINOR = 2;
 	
 	/** UNIX timestamp of the last commit */
-	public static final int COMMIT_TIME = 1645636699;
+	public static final int COMMIT_TIME = 1645648444;
 	
 	/** Branch name. Automatically changed by precommit.pl */
-	public static final String BRANCH = "dls";
+	public static final String BRANCH = "master";
 	
 	/** Full version string. */
 	public static final String VERSION = VERSION_MAJOR + "." + VERSION_MIDDLE + "." + VERSION_MINOR + ("master".equals(BRANCH) ? "" : "-" + BRANCH);
